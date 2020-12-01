5794e00071cb3b67edf2d0c46105fe5a776e8265
Taehoon Yoon <yooontehoon@naver.com>
2020-12-01 23:46:32 +0900

    번역 추가. 셸 스크립트 추가.

---
+++ update.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+./toutf.pl messages_ko_original.properties>messages_ko.properties
+cp /home/taehun/문서/tuxguitar번역/messages_ko.properties /opt/tuxguitar/share/lang/messages_ko.properties
